eb7e44c4ab78de15deb7ed79d38a5cf065150495
msakibhr <msakibhr@gmail.com>
2025-10-16 15:16:27 +0600

    refactor: uncomment MongoDB ping command and client close in run function

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
+// app.use(cors({
+//     origin: ["http://localhost:5173", "https://coffee-store-9b884.web.app", "https://coffee-store-9b884.firebaseapp.com"]
+// }));
 app.use(express.json());
 
 // const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
@@ -113,11 +116,11 @@ async function run() {
         })
 
         // Send a ping to confirm a successful connection
-        // await client.db("admin").command({ ping: 1 });
+        await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        // await client.close();
+        await client.close();
     }
 }
 run().catch(console.dir);
